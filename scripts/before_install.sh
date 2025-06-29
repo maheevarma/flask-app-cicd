5a3c52f959f28df784892b9164c443eab96b48c7
maheevarma <mickymahender@gmail.com>
2025-06-29 13:49:29 +0530

    Add cleanup logic to before_install

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-sudo systemctl stop flaskapp || true
+
+echo "🧹 Cleaning up old Python and log files in /home/ec2-user/flask-app..."
+
+find /home/ec2-user/flask-app -type f \( -name "*.py" -o -name "*.log" \) -exec rm -f {} \;
+
+echo "✅ Cleanup complete."
